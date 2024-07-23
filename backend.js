9c3b47e9b6ca54e13168852fd45211dae412a641
zvoverman <zvoverman@gmail.com>
2024-07-22 22:14:38 -0600

    change force calc.

--- backend.js
+++ backend.js
@@ -248,8 +248,8 @@ function normalizeVector(vector) {
 
 // Function to apply force to the player being hit
 function applyForce(player, forceVector, forceMagnitude) {
-    player.dx -= forceVector.x * forceMagnitude;
-    player.dy -= forceVector.y * forceMagnitude;
+    player.dx = forceVector.x * forceMagnitude;
+    player.dy = forceVector.y * forceMagnitude;
 }
 
 // Main function to handle the hit
